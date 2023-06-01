8161917afc74195669c087a1a2bb8dfe4c162248
danlgz <hi@danlgz.io>
2023-06-01 09:19:08 -0600

    [ADD] operations and equal functionality

--- week_09/day_04/session/app.js
+++ week_09/day_04/session/app.js
@@ -1,3 +1,13 @@
+// ======= UTILS ==========
+let memory = [];
+let selectedOperator = null;
+const equalHandler = () => {
+    const currentValue = display.innerHTML;
+    const result = eval(`${memory.join(' ')} ${currentValue}`);
+    setDisplayValue(result);
+    memory = [];
+}
+
 // ======= Display ==========
 const display = document.querySelector("#display");
 const setDisplayValue = (value) => {
@@ -33,24 +43,40 @@ numbers.forEach((element, index) => {
     element.addEventListener('click', () => {
         const currentValue = display.innerHTML;
         let newValue = `${currentValue}${index}`;
-        if (currentValue === '0') {
+        if (currentValue === '0' || selectedOperator !== null) {
             newValue = index;
         }
         setDisplayValue(newValue);
-    })
+
+        if (selectedOperator !== null) {
+            selectedOperator.classList.remove('selected');
+            selectedOperator = null;
+        }
+    });
 });
 
 // ======= AC ==========
 const ac = document.querySelector('#ac')
 ac.addEventListener('click', () => {
+    if (selectedOperator !== null) {
+        selectedOperator.classList.remove('selected');
+    }
     setDisplayValue(0);
+    memory = [];
 });
 
 // ======= SIGN ==========
 const sig = document.querySelector('#sig');
 sig.addEventListener('click', () => {
     let currentValue = Number(display.innerHTML);
-    setDisplayValue(currentValue * -1);
+    const inverseValue = currentValue * -1;
+    setDisplayValue(inverseValue);
+    if (memory.length === 2) {
+        memory = [
+            inverseValue,
+            memory.pop(),
+        ]
+    }
 });
 
 // ======= DOT ==========
@@ -61,3 +87,52 @@ dot.addEventListener('click', () => {
         setDisplayValue(`${currentValue}.`);
     }
 })
+
+// ======= PERCENT ==========
+const percent = document.querySelector('#percent');
+percent.addEventListener('click', () => {
+    let currentValue = Number(display.innerHTML);
+    currentValue /= 100;
+    setDisplayValue(currentValue);
+});
+
+// ======= OPERATORS ==========
+const div = document.querySelector('#div');
+const mul = document.querySelector('#mul');
+const sum = document.querySelector('#sum');
+const sub = document.querySelector('#sub');
+
+const operators = [
+    { el: div, sign: '/'},
+    { el: mul, sign: '*'},
+    { el: sum, sign: '+'},
+    { el: sub, sign: '-'}
+];
+
+operators.forEach((operator) => {
+    operator.el.addEventListener('click', () => {
+        let currentValue = Number(display.innerHTML);
+        if (memory.length === 0) {
+            memory.push(currentValue);
+        }
+
+        if (memory.length === 2 && selectedOperator !== null) {
+            memory.pop();
+            selectedOperator.classList.remove('selected');
+        }
+
+        if (memory.length === 2 && selectedOperator === null) {
+            equalHandler();
+            currentValue = Number(display.innerHTML);
+            memory.push(currentValue);
+        }
+
+        memory.push(operator.sign);
+        operator.el.classList.add('selected');
+        selectedOperator = operator.el;
+    });
+});
+
+// ======= EQUAL ==========
+const equal = document.querySelector('#equal');
+equal.addEventListener('click', equalHandler);
